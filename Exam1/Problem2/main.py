603ba290ea16403b72ae5d55d735eaa7bc2463bc
Zenif-NIght <41389677+Zenif-NIght@users.noreply.github.com>
2021-09-15 17:52:04 -0600

    Added cool stuff yo

--- Exam1/Problem2/main.py
+++ Exam1/Problem2/main.py
@@ -1,69 +1,136 @@
+import sys
+import gym
+# from gym.envs.classic_control.cartpole import CartPoleEnv
+from cartpole import CartPoleEnv
 
-
-
-
-# Python imports
 import numpy as np
-from time import time  # just to have timestamps in the files
+# from simple_plotter import Plotter
 import matplotlib.pyplot as plt
 
-# OpenAI Gym imports
-import gym
-from gym import wrappers
+
+class SingleValuePID():
+    def __init__(self, expect_value, kp, ki, kd):
+        self.expect_value = expect_value
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.prev_delta = 0
+        self.integral_delta = 0
+
+    def output(self, actual_value):
+        delta = self.expect_value - actual_value
+
+        p_comp = delta * self.kp
+        self.integral_delta += delta
+        i_comp = self.integral_delta * self.ki
+        d_comp = (delta - self.prev_delta) * self.kd
+        self.prev_delta = delta
+        output = p_comp + i_comp + d_comp
+        return output
 
 
-class MathAgent:
-    def __init__(self, environment):
-        self.environment = environment
+class SolverPID():
+    def __init__(self, expect_position, expect_angle,
+            kp_position, ki_position, kd_position,
+            kp_angle, ki_angle, kd_angle):
+        self.position_pid = SingleValuePID(expect_position,
+            kp_position, ki_position, kd_position)
+        self.angle_pid = SingleValuePID(expect_angle,
+            kp_angle, ki_angle, kd_angle)
 
-    def act(self, _):
-        return self.environment.action_space.sample()
+    def choose(self, state):
+        position = state[0]
+        velocity = state[1]
+        angle = state[2]
+        pole_velocity = state[3]
 
-# Remove the monitoring if you do not want a video
-env = gym.make("CartPole-v1").env
-env._max_episode_steps = 10000
-env = wrappers.Monitor(env, "./videos/" + str(time()) + "/")
+        print(("position", position))
+        print(("velocity", velocity))
+        print(("angle", angle))
+        print(("pole_velocity", pole_velocity))
+
+        #action = np.random.randint(2)
+        
+        pos_output = self.position_pid.output(position)
+        angle_output = self.angle_pid.output(angle)
+        action = 1 if (angle_output + pos_output) < 0 else 0
+        
+        print(action)
+        return action
 
 
-# Change the agent to a different one by simply swapping out the class
-# ex) RandomAgent(env) --> TabularAgent(env)
-agent = MathAgent(env)
-#agent = TabularAgent(env)
+if __name__ == "__main__":
+    '''
+    https://github.com/openai/gym/blob/master/gym/envs/__init__.py
+    register(
+        id='CartPole-v1',
+        entry_point='gym.envs.classic_control:CartPoleEnv',
+        max_episode_steps=500,
+        reward_threshold=475.0,
+    )
+    '''
+    # env = gym.make("CartPole-v1")
+    env = CartPoleEnv()
 
-# We are only doing a single simulation. Increase 1 -> N to get more runs.
-for iteration in range(1):
-    #print(iteration)
-    # Always start the simulation by resetting it
+    print(env.action_space)
+    print(env.observation_space)
+    print(env.observation_space.low)
+    print(env.observation_space.high)
+
     state = env.reset()
     done = False
+    total_reward = 0
 
-    # Either limit the number of simulation steps via a "for" loop, or continue
-    # the simulation until a failure state is reached with a "while" loop
-    while not done:
+    input_value = float(1000)#100)#sys.argv[1])
+    print(input_value)
 
-        # Render the environment. You will want to remove this or limit it to the
-        # last simulation iteration with a "if iteration == last_one: env.render()"
-        
+    # (2, 0, 50)
+    kp_position = 0
+    ki_position = 0
+    kd_position = 0
+    
+    # (-8, 0, -100)
+    kp_angle = 5
+    ki_angle = 0
+    kd_angle = input_value
+
+    algo = SolverPID(0, 0,
+        kp_position, ki_position, kd_position,
+        kp_angle, ki_angle, kd_angle)
+
+    # plotter = Plotter()
+    data = [[],[],[],[]]
+    time_steps = []
+    time = 0
+    while (not done):
         env.render()
 
-        # Have the agent
-        #   1: determine how to act on the current state
-        #   2: go to the next state with that action
-        #   3: learn from feedback received if that was a good action
-        action = agent.act(state)
-        next_state, reward, done, _ = env.step(action)
-       
+        action = algo.choose(state)
+        data[0].append(state[0])
+        data[1].append(state[1])
+        data[2].append(state[2])
+        data[3].append(state[3])
+        time_steps.append(time)
+        time += 1
+        new_state, reward, done, _ = env.step(action)
 
-        # Progress to the next state
-        state = next_state
+        state = new_state
+        total_reward += reward
 
-    #env.render()  # Render the last simluation frame.
-env.close()  # Do not forget this! Tutorials like to leave it out.
 
-"""
-MountainCar Movements
-    0 - Move left
-    1 - Don't move
-    2 - Move right
-"""
-#print(agent.value_table)
+    env.close()
+    
+    # plt.figure(figsize=(14,6))
+    figure, axis = plt.subplots(2, 1)
+    axis[0].plot(time_steps, data[0][:], "b")
+    axis[0].plot(time_steps, data[1][:], "r")
+    axis[1].plot(time_steps, data[2][:], "g")
+    axis[1].plot(time_steps, data[2][:], "y")
+    
+    axis[0].legend(['position','velocity'])
+    
+    axis[1].legend(['angle', 'pole_velocity'])
+
+
+    plt.plot()
+    print(("total reward = ", total_reward))
