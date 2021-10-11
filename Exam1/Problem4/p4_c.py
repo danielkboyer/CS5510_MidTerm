adfb123ef2419c57489ec4b46170188d5102e38a
Jaxon Willard <jaxonbwillard@gmail.com>
2021-10-11 17:40:10 -0600

    set up p4

--- Exam1/Problem4/p4_c.py
+++ Exam1/Problem4/p4_c.py
@@ -3,13 +3,15 @@ import numpy as np
 def get_h_matrix(theta, length):
     c = np.cos(theta)
     s = np.sin(theta)
-    return np.matrix([
+    mt =  np.matrix([
         [c, -s, 0, length*c],
         [s,  c, 0, length*s],
         [0,  0, 1, 0],
         [0,  0, 0, 1]
         ])
+    return mt
 
 a0 = get_h_matrix(np.pi/6, 0.6)
 a1 = get_h_matrix(np.pi/4, 0.4)
-print(np.dot(a0, a1))
+print(np.dot(a0, a1))    
+print(np.matmul(a0, a1))
