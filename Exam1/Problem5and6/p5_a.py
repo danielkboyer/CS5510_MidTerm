7b3ae038d1ff940bad38657ec28ce38290033800
Jaxon Willard <jaxonbwillard@gmail.com>
2021-10-14 14:46:43 -0600

    change iterations on p5_a

--- Exam1/Problem5and6/p5_a.py
+++ Exam1/Problem5and6/p5_a.py
@@ -3,4 +3,4 @@ from utils import find_inverse_kinematics
 
 goal = np.array((1.2, 0.8, 0.5))
 variables = np.array([0.0,0.0,0.0,0.0,0.0,0.0]) # t1, d2, d3, t4, t5, t6
-find_inverse_kinematics(goal, variables, 40000)
+find_inverse_kinematics(goal, variables, 10000)
